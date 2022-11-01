a362d4bc1ba88d5695acaf1540d184492454c20e
sky <sky012877@gmail.com>
2022-11-01 15:17:42 -0700

    create VM tools finished

--- virtual_box/create_vm.sh
+++ virtual_box/create_vm.sh
@@ -10,6 +10,10 @@ vm_name=Manjaro_KDE
 os_type=Linux_64
 hdd_size=50000
 memory=8192
+videoram=128
+graphcontr=vboxsvga
+accel3D=on
+iso_file=~/Downloads/manjaro-kde.iso
 
 #vms_directory=~/VirtualBox VMs/
 
@@ -27,22 +31,41 @@ VBoxManage modifyvm $vm_name --nic1 bridged --bridgeadapter1 eno2 --nictype1 825
 echo Choosing Virtual machine OS Type...
 VBoxManage modifyvm $vm_name --ostype $os_type
 
+echo Setup $memory of memory... 
+VBoxManage modifyvm $vm_name --memory $memory
+
+echo Setup video memory to $videoram
+VBoxManage modifyvm $vm_name --vram $videoram
+
+echo Setup graphic controller to $graphcontr
+VBoxManage modifyvm $vm_name --graphicscontroller=$graphcontr
+echo Setup 3D acceleration = $accel3D
+VBoxManage modifyvm $vm_name --accelerate3d=$accel3D
+
 dir=`VBoxManage showvminfo $vm_name | grep "Config file"`
 dir="/${dir#*/}"
 dir="${dir%/*}/"
-# echo $dir
+echo $dir
+cd "$dir"
+hdd_file="$dir$vm_name.vdi"
 
-echo Setup $memory of memory 
-VBoxManage modifyvm $vm_name --memory $memory
 
-echo Creating HDD $dir$vm_name.vdi with size = $hdd_size ...
-VBoxManage createhd --filename "$dir$vm_name.vdi" --size $hdd_size
-
-cd $dir
+echo Creating HDD $hdd_file with size = $hdd_size ...
+VBoxManage createhd --filename "$hdd_file" --size $hdd_size
 ls 
 
-VBoxManage showvminfo $vm_name
-
-echo Adding IDE controller
-
+echo Adding IDE controller ...
 VBoxManage storagectl $vm_name --name "IDE Controller" --add ide
+
+echo Connecting HDD to IDE controller...
+VBoxManage storageattach $vm_name --storagectl "IDE Controller"  --port 0 --device 0 --type hdd --medium "$hdd_file"
+
+echo Connecting ISO file...
+
+VBoxManage storageattach $vm_name --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium $iso_file
+
+
+VBoxManage showvminfo $vm_name 
+read -p "Any key for continue to START VM : " any_key
+echo Starting VM...
+VBoxManage startvm $vm_name
