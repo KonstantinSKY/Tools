d4c4822506c9d112532e20857213fd594fb3557b
sky <sky012877@gmail.com>
2022-11-02 10:06:52 -0700

    added setup for git

---
+++ git/setup_git.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+############################################################################
+# Script name : setup_git.sh                 Date   : 11/02/22             #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Setup git                                                  #
+############################################################################
+
+# Variables from env
+name=$USER_NAME
+email=$EMAIL
+
+addIgnored = false
+
+echo Setting for git client...
+echo Checking availibility...
+which git
+
+echo Git config ./gitconfig...
+cat ~/.gitconfig
+git config --list
+
+echo new seting:
+echo name: $name
+echo email: $email
+read -p "Any key for continue:" anykey
+
+echo Your new name $name
+git config --global user.name $name
+git config user.name
+
+echo  Your new email address: $email
+git config --global user.email $email
+git config user.email
+
+git config advice.addIgnoredFile $addIgnired
+
+echo Result:
+
+git config --list
+
