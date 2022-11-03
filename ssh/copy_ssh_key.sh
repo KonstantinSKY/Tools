72853604ec3de214eed85cf5aa7ecf753201cbda
sky <sky012877@gmail.com>
2022-11-02 20:40:34 -0700

    added copy ssh key tool

---
+++ ssh/copy_ssh_key.sh
@@ -0,0 +1,23 @@
+
+
+
+echo Copy ssh key to:
+
+name=$NAME
+ip="192.168.0.75"
+port=22
+
+
+read -p "Enter the new name, like $name: " name
+read -p "Enter the new ipname, like $ip: " ip
+read -p "Enter the new port, like $port: " port
+comm="ssh-copy-id -p $port $name@$ip"
+
+echo Copying ssh key by command : $comm
+
+$comm
+
+echo Trying to connect to $ip
+
+ssh $name@$ip -p $port
+
