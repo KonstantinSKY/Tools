68e64d2c49d326068f023154efbf9c1fe0181511
SKY <sky012877@gmail.com>
2022-11-05 00:28:29 +0000

    status.sh - Added and modified :test 1

---
+++ git/status.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+echo Git status :
+git status
+git status -s
+
+str_arr=(`git status -s`)
+
+#echo status array : ${str_arr[@]}
+#echo indexes : ${!str_arr[@]}
+
+for idx in ${!str_arr[@]}
+do
+	[ $(($idx % 2)) -eq 0 ] && f_type=${str_arr[$idx]} || continue 
+	file=${str_arr[$idx+1]}	
+	case $f_type in 
+		M)
+			message="Modified" ;;
+		AM)
+			message="Added and modified" ;;
+
+	esac
+	
+	echo ========================================================
+	echo "$file - $message :"
+	echo Gonna commit for $file:
+	echo "* Text your message for the commit *"
+	read -e -p " " -i "$file - $message :" u_message
+	echo Commiting ...
+#	echo $u_message
+	git commit -m "$u_message" $file
+
+done
+
+git status
+
