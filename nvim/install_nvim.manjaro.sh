3d52b36ed4419832d24604d92879fe0254379945
SKY <sky012877@gmail.com>
2022-11-07 03:17:58 +0000

    install_nvim.manjaro.sh - Modified : finished

--- nvim/install_nvim.manjaro.sh
+++ nvim/install_nvim.manjaro.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 
-echo_g "Install and setup nvim and plugins"
-echo_g "Installing Neovim..."
+echo "Install and setup nvim and plugins for Manjaro desktop Linux"
+echo "Installing Neovim..."
 
 update.sh
 sudo pacman -S neovim
 
-echo_g "Set vim.init and create symbolic link"
+echo "Installing vim-plug pluging manager for neovim ..."
 
-
-echo_g "Install plugin manager ..."
 sudo pamac install neovim-plug
+
+read "Any key for setup nvim and install plugins" anykey
+.setup_nvim.sh
 
