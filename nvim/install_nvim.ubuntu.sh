851abd655ad3312baf0054bf21344f9192b04b9b
SKY <sky012877@gmail.com>
2022-11-07 03:18:08 +0000

    install_nvim.ubuntu.sh - Modified : finished

--- nvim/install_nvim.ubuntu.sh
+++ nvim/install_nvim.ubuntu.sh
@@ -12,14 +12,5 @@ echo "Installing vim-plug  for neovim..."
 sh -c curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim -Q-create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-read "Any key for setup nvim and install plugin" anykey
+read "Any key for setup nvim and install plugins" anykey
 . setup_nvim.sh
-
-echo "Set vim.init and create symbolic link"
-
-mkdir -vp ~/.config/nvim/
-rm -iv ~/.config/nvim/init.vim
-ln -s ~/Tools/nvim/init.vim ~/.config/nvim/init.vim
-
-echo "Install Plugins ..."
-nvim -c 'PlugInstall' -c ':x' -c ':x'
