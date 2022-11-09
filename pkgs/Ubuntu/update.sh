dc3b159863206cfae72e8daddd941ba314053d2f
SKY <sky012877@gmail.com>
2022-11-09 00:54:04 +0000

    pkgs/Ubuntu/update.sh - Added :

---
+++ pkgs/Ubuntu/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Update system Ubuntu                                       #
+############################################################################
+
+sudo apt update
+sudo apt upgrade
