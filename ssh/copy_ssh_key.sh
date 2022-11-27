ce4cfb5b1d813d2a7a3c8a2998af4af0cd110c3c
sky <sky012877@gmail.com>
2022-11-26 20:52:28 -0800

    copy_ssh_key.sh - Modified :

--- ssh/copy_ssh_key.sh
+++ ssh/copy_ssh_key.sh
@@ -1,23 +1,23 @@
 
 
 
-echo Copy ssh key to:
+echo Copy ssh key to remote PC:
 
 name=$NAME
 ip="192.168.0.75"
 port=22
 
 
-read -p "Enter the new name, like $name: " name
+read -p "Enter the new ssh user name, like $name: " name
 read -p "Enter the new ipname, like $ip: " ip
 read -p "Enter the new port, like $port: " port
 comm="ssh-copy-id -p $port $name@$ip"
 
-echo Copying ssh key by command : $comm
+echo Copying ssh key by command : $comm ...
 
 $comm
 
-echo Trying to connect to $ip
+echo Trying to connect to $ip ...
 
 ssh $name@$ip -p $port
 
