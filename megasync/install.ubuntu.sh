f741045adfa26789b4fcdcd4474e424f3c68084b
sky <sky012877@gmail.com>
2022-11-30 09:42:32 -0800

    install.ubuntu.sh - Added :

---
+++ megasync/install.ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Install megacmd for ubuntu server                          #
+############################################################################
+
+
