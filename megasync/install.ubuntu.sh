443440f7cb4efc1489d47b51e7937a9aca2ca4e2
sky <sky012877@gmail.com>
2022-11-30 18:29:18 +0000

    install.ubuntu.sh - Modified :

--- megasync/install.ubuntu.sh
+++ megasync/install.ubuntu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 ############################################################################
 # Script name :                              Date   :                      #
@@ -6,4 +6,29 @@
 # Description : Install megacmd for ubuntu server                          #
 ############################################################################
 
+inst_file=$PWD/megacmd.deb
+echo $inst_file
+
+ echo Updatind Linux system ...
+sudo apt update
+sudo apt upgrade
+
+ echo Installing megacmd for comand line ...
+ echo Checking linux version
+
+ lsb_release -a
+
+echo Getting instalation file
+echo Will gett file for https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megacmd-xUbuntu_22.04_amd64.deb
+echo Anykey if u wanna to continue instalation file
+read anykey
+
+wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megacmd-xUbuntu_22.04_amd64.deb -O $inst_file
+
+sudo apt install $inst_file
+
+echo Removing $inst_file
+rm $inst_file
+
+echo Done!
 
