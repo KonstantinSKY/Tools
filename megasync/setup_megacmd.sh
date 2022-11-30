b5c4a9cd71e286cdf49d8d5a6a4f35072b1c8813
sky <sky012877@gmail.com>
2022-11-29 16:23:27 -0800

    megasync/setup_megacmd.sh - Added :

---
+++ megasync/setup_megacmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Install megasync and megacmd for Manjaro Linux             #
+############################################################################
+
+echo Setting megacmd
+read -p "Login: " login
+read -p "Password: " -s password
+
+
+mega-login $login $password
+mega-whoami 
+echo Remote directories:
+mega-ls
+
+echo 'Use "mega-sync" command to add syncing directory pairs'
+echo mega-sync [local_directory] [remote directory]
+echo After use mega-sync one more time without arguments 
+echo Done!
