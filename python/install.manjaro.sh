de203b8d2fc296b4f1cd50f9998e27cac22b3a7a
sky <sky012877@gmail.com>
2022-11-30 23:35:26 -0800

    python/install.manjaro.sh - Added : y

---
+++ python/install.manjaro.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Install and setup python for manjaro                       #
+############################################################################
+
+echo Installing Python 3.11
+upd
+sudo pamac install python311
+
+
+echo list of installed Python alternatives in /usr/bin/
+ls /usr/bin | grep python
+list=($(ls /bin/ | grep python))
+
+for variant in ${list[@]}
+	do
+		echo Start Version for : $variant
+		$variant --version
+	done
+
+echo Anykey if u wanna continue update alternatives
+
+read anykey
+cd /usr/bin/
+
+sudo rm python
+sudo rm python-config
+
+sudo ln -s python3.11 python
+sudo ln -s python3.11-config python-config
+ls -a --color | grep python
+
+
+cd -
+echo check python
+python --version
+echo check python3
+python3 --version
+
+
