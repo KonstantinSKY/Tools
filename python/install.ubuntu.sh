98c4c00a2c9c8ca632b372791989d2c53d4a2c75
SKY <sky012877@gmail.com>
2022-12-01 06:06:06 +0000

    install.ubuntu.sh - Modified :

--- python/install.ubuntu.sh
+++ python/install.ubuntu.sh
@@ -6,19 +6,33 @@
 # Description : Install and setup python for ubuntu                        #
 ############################################################################
 
+echo Installing python 3.11
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install python3.11 -y
+
+
+
 echo list of installed Python alternatives in /usr/bin/
 ls /usr/bin | grep python
 list=($(ls /bin/ | grep python))
 
-#echo ${list[@]}
-
 for variant in ${list[@]}
 	do
-		echo Starting Version for : $variant
+		echo Start Version for : $variant
 		$variant --version
 	done
 
-echo Anykey if u wanna continue
+echo Anykey if u wanna continue update alternatives
 
-apt list | grep python
+read anykey
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+
+echo check python
+python --version
+echo python3
+python3 --version
+
 
