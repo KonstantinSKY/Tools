0e798321893967c87a37e6f65705f2f51d713d0c
SKY <sky012877@gmail.com>
2022-12-01 18:16:57 +0000

    pip_install.manjaro.sh - Modified :

--- python/pip_install.manjaro.sh
+++ python/pip_install.manjaro.sh
@@ -3,19 +3,27 @@
 
 echo_g "# Python install & setup tool #"
 echo_g "Update and upgrade system and packages ..."
-#sh update.sh
 
-echo_g "Installed:"
+. /update.sh
+
+echo_g "Checking for installing versions ..."
+echo Python:
 python -V
-echo_g "pip installing"
-python -m pip install
+echo pip:
 python -m pip --version
 
+echo Anykey for install and upgrade python pip
 read anykey
-
+echo Installing pip ...
 sudo pacman -S python-pip
 python3 -m pip --version
+echo Upgrading pip...
 sudo pip install --upgrade pip
+python3 -m pip --version
+
+echo Anykey for install and setting pip-review
+read anykey
+echo installing pip-review ...
 sudo pip install pip-review
 pip-review --auto
-echo -e "Done $cn"
+echo -e "Done! $cn"
