4fe861562c207af39dbef1c95422c09d558cd3a3
sky <sky012877@gmail.com>
2022-12-01 20:17:31 -0800

    ssh/remote_start_sudo_script.sh - Added :

---
+++ ssh/remote_start_sudo_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Start script with sudo rights at remote host               #
+############################################################################
+echo script $1
+if [ ! $1 ];
+then
+	echo Add the script name at remote machine as argument! Continuation is impossible. Exit.
+	exit
+fi
+script=$1
+
+echo Remote starting for script: $script
+
+. get_hostname.sh
+
+echo Enter sudo password for remote host
+read -s password
+
+ssh $hostname "echo '$password' | sudo -Sv && bash -s" < $script
+
+echo Done!
