27a9e57928ae49ca4ff5855562ff56bf3af999e2
sky <sky012877@gmail.com>
2022-12-01 20:17:09 -0800

    pkgs/Ubuntu/update.sh - Modified :

--- pkgs/Ubuntu/update.sh
+++ pkgs/Ubuntu/update.sh
@@ -5,6 +5,26 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description : Update system Ubuntu                                       #
 ############################################################################
+echo Updating Ubuntu...
+sudo apt update -y
+echo
+echo Upgradable package list:
+apt list --upgradable
 
-sudo apt update
-sudo apt upgrade
+echo
+echo Upgrading Ubuntu
+sudo apt upgrade -y
+
+echo
+echo Updating Ubuntu...
+sudo apt update -y
+
+echo Any key for autoremove unused packages
+read anykey
+
+echo
+sudo apt autoremove
+
+echo
+echo Updating Ubuntu...
+sudo apt update -y
