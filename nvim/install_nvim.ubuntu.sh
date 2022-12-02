608ed39ec9cb8c6763324b4078a27b0de53121eb
sky <sky012877@gmail.com>
2022-12-01 21:34:44 -0800

    install_nvim.ubuntu.sh - Modified :

--- nvim/install_nvim.ubuntu.sh
+++ nvim/install_nvim.ubuntu.sh
@@ -2,9 +2,10 @@
 
 
 echo "Install and setup nvim and plugins for ubuntu server"
-echo "Installing Neovim..."
 
-sudo apt update && sudo apt upgrade
+. upd.sh
+
+echo "Installing Neovim..."
 sudo apt install neovim
 
 echo "Installing vim-plug  for neovim..."
@@ -12,8 +13,8 @@ echo "Installing vim-plug  for neovim..."
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo Set vimrc with 8 colors
-cp init.vim.8_colors init.vim
+#echo Set vimrc with 8 colors
+#cp init.vim.8_colors init.vim
 
 read -p "Any key for setup nvim and install plugins" anykey
 echo Starting setup_nvim.sh
