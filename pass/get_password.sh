61ced7b10d0cff40c28994f15b738fba3a7d3184
SKY <sky012877@gmail.com>
2022-12-12 02:48:45 +0000

    get_password.sh - Added :

---
+++ pass/get_password.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description :                                                            #
+############################################################################
+
+echo Getting password or any line from pass
+
+pass
+
+[ $1 ] && pass_name=$1 || read -p " Enter the pass name " pass_name
+
+echo pass_name: $pass_name
+
+
+[ $2 ] && line=$2 || read -p " Enter the line number " line
+
+echo line number: $line
+
+passwords=(`pass $pass_name`)
+
+echo ${passwords[@]}
+password=${passwords[$line]}
+
+echo password: $password
+
+#for lane in $password:
+#do
+#	echo $lane
+#done
+
