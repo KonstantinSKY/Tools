ea48ba1e3d658aa2dd56c818ba62087d7b77fcec
sky <sky012877@gmail.com>
2022-12-29 20:10:21 -0800

    install_pkgs.sh - Modified :

--- pkgs/Manjaro/install_pkgs.sh
+++ pkgs/Manjaro/install_pkgs.sh
@@ -5,13 +5,12 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description : Install packages from file list                            #
 ############################################################################
-
-sudo pamac update -a
+sudo pamac upgrade --force-refresh --aur
 readarray -t packages < $1
 echo Will be installed
 echo ${packages[@]}
 read -p "Anykey if You want to install all these packages" anykey
-sudo pamac install -a ${packages[@]}
+sudo pamac install ${packages[@]}
 
 
 
