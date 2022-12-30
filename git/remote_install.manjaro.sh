53d166cbe4fc2b9d9f8891734b33b014bc3d0099
SKY <sky012877@gmail.com>
2022-12-30 00:42:59 +0000

    remote_install.manjaro.sh - Added :

---
+++ git/remote_install.manjaro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Remote install git programs for manjaro                    #
+############################################################################
+
+echo Installing git programs..
+. remote_start_sudo_script.sh install.manjaro.sh
+echo Done !
