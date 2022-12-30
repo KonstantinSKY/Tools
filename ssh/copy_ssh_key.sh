efe5a70284f288d194fe3b6103a7c59416b605fd
SKY <sky012877@gmail.com>
2022-12-30 00:00:10 +0000

    copy_ssh_key.sh - Modified :

--- ssh/copy_ssh_key.sh
+++ ssh/copy_ssh_key.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo Copy ssh key to remote PC:
-. /get_hostname.sh
+. get_hostname.sh
 
-read -p "Anykey if U wanna copy ssh-key to:  $hostname" anykey
+read -p "Anykey if You wanna copy ssh-key to:  $hostname" anykey
 
 echo Copying ssh key to $hostname ...
 ssh-copy-id $hostname
