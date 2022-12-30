dd586747a0cb1531e310f037740a10f70885c36d
sky <sky012877@gmail.com>
2022-12-29 16:01:21 -0800

    ssh/gen_ssh_key.sh - Modified :

--- ssh/gen_ssh_key.sh
+++ ssh/gen_ssh_key.sh
@@ -6,9 +6,6 @@ key_file=$SSH_KEY
 echo Generating SSH keys....
 ssh-keygen -t ed25519 -f $key_file -C $email
 
-# Example for copy
-# ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -C XXXX@gmail.com
-
 echo $key_file key: \n
 
 cat $key_file.pub
