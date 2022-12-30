453c8e572e97e8c77d8ea4545acb7b3106bcc91b
sky <sky012877@gmail.com>
2022-12-29 18:33:54 -0800

    create_user.sh - Modified :

--- users/create_user.sh
+++ users/create_user.sh
@@ -7,7 +7,7 @@
 ############################################################################
 
 . colors.sh
-passw=/etc/passwd
+passwd=/etc/passwd
 
 echo -e "$cg Create new non admin user $cn"
 echo Existing users in $passwd
@@ -16,7 +16,10 @@ cat $passwd
 echo -e "$cv Enter the user name: $cn"
 read username
 
+echo Creaating new user $username
 sudo useradd -m -s /bin/bash $username
+
+echo User in $passwd:
 cat $passwd | grep $username
 echo Home directory:
 ls /home/
