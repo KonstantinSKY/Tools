8caaa494f0770ca5d902ed690c90d98e80f25444
SKY <sky012877@gmail.com>
2023-01-01 21:10:27 -0800

    vms/install.manjaro.sh - Modified :

--- vms/install.manjaro.sh
+++ vms/install.manjaro.sh
@@ -11,3 +11,12 @@ sudo pacman -Syu
 
 echo Installing Virtual box
 sudo pacman -Syy virtualbox
+
+echo You can need to install VM package for you Linux kernel
+mhwd-kernel -li
+
+echo sudo pacman -Syu virtualbox linuxXX-virtualbox-host-modules
+echo sudo vboxreload
+echo 'see https://wiki.manjaro.org/index.php/VirtualBox#Installing_Virtualbox_on_Manjaro'
+
+
