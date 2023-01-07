3a69e2755fc928b4d6cb36452a08135564467863
SKY <sky012877@gmail.com>
2023-01-06 21:20:26 -0800

    sdsa :q

---
+++ webdrivers/chrome_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description :                                                            #
+############################################################################
+
+ver='108.0.5359.71'
+
+echo Chromedriver installing
+echo google chrome version:
+
+google-chrome-stable --version
+echo Will be install chromedriver version: $ver
+google-chrome-stable https://chromedriver.chromium.org/downloads
+
+echo getting file from https://chromedriver
+wget https://chromedriver.storage.googleapis.com/version/chromedriver_linux64.zip
+
+unzip chromedriver_linux64.zip
+sudo chmod +x chromedriver
+sudo mv chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
+chromedriver --version
+rm chromedriver_linux64.zip
+
