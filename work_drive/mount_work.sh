8de7729f537030f2dbfb1df4bc1cd5ab689e0ad5
sky <sky012877@gmail.com>
2023-01-29 20:39:03 -0800

    work_drive/mount_work.sh - Modified :

--- work_drive/mount_work.sh
+++ work_drive/mount_work.sh
@@ -5,25 +5,27 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description :                                                            #
 ############################################################################
-dir=$HOME/Work
-params=default
+dir=$HOME/Work/
+params="default 0 0"
+fs=btrfs
 mkdir -p $dir
 echo Mounting Work disk to $dir
 echo Enter the sudo password:
 read -s password
 echo $password
 drive=$(echo $password | sudo -S blkid | grep 'LABEL="Work"')
-echo  Drive : 
+echo  Drive :
 echo $drive
 
 drive_name=$(echo $drive | cut -f1 -d":")
 echo Anykey for mount $drive_name to $dir with parameters: $params
 read anykey
-sudo mount $drive_name $dir -t btrfs -o $params
+sudo mount $drive_name $dir #:q-t fs -o $params
 
 echo Cheking fstab ...
 cat /etc/fstab
 
+sudo findmnt --verify
 echo Done
 #echo Drive_data = $(echo $password | sudo -S blkid $drive_name)
 
