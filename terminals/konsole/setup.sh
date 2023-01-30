88f588d66fe1da3a3eb8b11a2543362efd62fa85
SKY <sky012877@gmail.com>
2023-01-29 20:52:07 -0800

    terminals/konsole/setup.sh - Modified :

--- terminals/konsole/setup.sh
+++ terminals/konsole/setup.sh
@@ -8,11 +8,12 @@
 dir_from=$(realpath $(dirname $0))
 
 echo Start from directory: $dir_from
-dir_to=$(realpath ~/.local/share/konsole)
+dir_to=$HOME/.local/share/konsole
 
 echo Creating symlinks...
 ln -s $dir_from/bash.profile $dir_to/bash.profile
 ln -s $dir_from/zsh.profile $dir_to/zsh.profile
+ln -s $dir_from/K_zsh.profile $dir_to/K_zsh.profile
 echo Created in $dir_to:
 ls  $dir_to/
 
