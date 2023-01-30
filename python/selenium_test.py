56deec0220f5296aa7bc05cfb2418059755ce19b
SKY <sky012877@gmail.com>
2023-01-29 20:52:06 -0800

    python/selenium_test.py - Modified :

--- python/selenium_test.py
+++ python/selenium_test.py
@@ -8,14 +8,15 @@ print("Install and test web driver")
 
 import time
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 
 driver.get('http://www.google.com/');
 time.sleep(5) # Let the user actually see something!
-search_box = driver.find_elements(by=name, 'q')
+search_box = driver.find_element(By.TAG_NAME, 'input')
 search_box.send_keys('ChromeDriver')
 search_box.submit()
-time.sleep(5) # Let the user actually see
+print("Done! wait 15 min")
+time.sleep(15) # Let the user actually see
