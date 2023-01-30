97a54c6b8f11b39ca5a7e10f72a1fd0273a88388
SKY <sky012877@gmail.com>
2023-01-29 20:52:07 -0800

    terminals/urxvt/setup_perl_ext.sh - Added :

---
+++ terminals/urxvt/setup_perl_ext.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description :                                                            #
+############################################################################
+
+mkdir -pv ~/.urxvt/ext/
+ln -svf $PWD/urxvt-perls/keyboard-select $HOME/.urxvt/ext/keyboard-select
+
+
