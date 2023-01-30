5ca0f1db22d6c4e7e5f46eed04791aae2678289e
SKY <sky012877@gmail.com>
2023-01-29 20:51:56 -0800

    configs/copy_i3.profile.sh - Added :

---
+++ configs/copy_i3.profile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description :                                                            #
+############################################################################
+
+echo Copying .profile ...
+cp -vf i3.profile ~/.profile
+echo Done!
