332f3e07510de1dcc8d20723fcd27a8f9f3809ed
SKY <sky012877@gmail.com>
2023-02-05 11:55:30 -0800

    auth_gh.sh - Added :

---
+++ git/auth_gh.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Remote login to github and add the ssh-key GitHub          #
+############################################################################
+
+gh_host=github.com
+file_path=$HOME/Security/gh_token.txt
+
+echo gh authentification
+echo Login status:
+gh auth status
+
+echo Any key if u sure to authehtificate to gh
+read anykey
+
+echo Getting gh token...
+. ~/Security/get_gh_token.sh
+
+
+[[ -z $token ]] && { echo "Enter the Token for github:";
+					read token;	} || echo "Token got"
+
+gh config set -h $gh_host git_protocol ssh
+
+echo Logining to gh with token: $token...
+gh auth login --with-token < $file_path
+
+rm $file_path
+echo Login status:
+gh auth status
+
+echo List of SSH keys in GitHub account:
+gh ssh-key list
+
+echo List of repos:
+gh repo list
+
+echo Done
