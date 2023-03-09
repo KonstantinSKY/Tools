c838701f39f4b87f2068aff4e3546f0cf2d29aa7
SKY <sky012877@gmail.com>
2023-03-08 16:22:02 -0800

    new_repo_clone.sh - Modified :

--- git/new_repo_clone.sh
+++ git/new_repo_clone.sh
@@ -3,36 +3,27 @@
 ############################################################################
 # Script name :                              Date   :                      #
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
-# Description :                                                            #
+# Description : Create new repository and directory                        #
 ############################################################################
 
 dir=$PWD
+license='GNU General Public Licence v3.0'
 
 echo Creating new repo at Github and clone to $dir
+echo License: $license
 
+gh repo list
 echo Enter repo name
 read repo
 echo Enter repo description
-read description
+read descr
 
 echo Will be create new public repo at GitHub: $dir/$repo
-echo Anykey for create new repo
+echo Anykey for create new repository
 read anykey
 
-gh repo create $repo --public --clone --add-readme -d "$description"
+gh repo create $repo --public --clone --add-readme -l $license -d "$descr"
 ls -la
 
 echo all repos listing...
-
 gh repo list
-#echo First commit
-
-cp $HOME/Tools/git/gitignore.template $dir/$repo/.gitignore
-cd $dir/$repo
-ls
-#git commit -m "First commit"
-git pull origin main
-git push
-ls -al
-echo Done
-
