903053f57e3713520d2ed52f9c45458964b7fb73
SKY <sky012877@gmail.com>
2023-03-28 10:50:26 -0700

    tests/README.sh - Added :

---
+++ helpers/tests/README.sh
@@ -0,0 +1,49 @@
+# Bash Script: t_color.sh
+
+This Bash script is designed to test the color codes used in Bash scripting.
+
+## Script Information
+
+- Script Name: t_color.sh
+- Author: Stan SKY
+- Email: sky012877@gmail.com
+- Date: 2023-03-27
+
+## Script Description
+
+The purpose of this script is to test the color codes used in Bash scripting. It defines various color codes as variables and then tests each variable to ensure that it has been defined correctly.
+
+## Color Tests
+
+The script tests the following color codes:
+
+- T_Black
+- T_R
+- T_G
+- T_Y
+- T_B
+- T_P
+- T_C
+- T_W
+- B_Black
+- B_R
+- B_G
+- B_Y
+- B_B
+- B_P
+- B_C
+- B_W
+- U_Black
+- U_R
+
+For each color code, the script checks whether the variable has been defined correctly. If a variable is not defined correctly, the script will output an error message and exit with a status code of 1.
+
+## Usage
+
+To run the script, execute the following command:
+```
+./t_color.sh
+```
+
+
+
