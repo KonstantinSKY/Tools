84c3500a19563aee15e306ba79feb945eab2f515
SKY <sky012877@gmail.com>
2023-03-28 01:39:19 -0700

    colors.sh - Modified :

--- helpers/colors.sh
+++ helpers/colors.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-############################################################################
-# Script name :                              Date   :                      #
-# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
-# Description : for colorize text string                                   #
-############################################################################
+# Text color variables
+T_Black='\033[0;30m'        # Black
+T_R='\033[0;31m'            # Red
+T_G='\033[0;32m'            # Green
+T_Y='\033[0;33m'            # Yellow
+T_B='\033[0;34m'            # Blue
+T_P='\033[0;35m'            # Purple
+T_C='\033[0;36m'            # Cyan
+T_W='\033[0;37m'            # White
 
+# Bold text color variables
+B_Black='\033[1;30m'        # Black
+B_R='\033[1;31m'            # Red
+B_G='\033[1;32m'            # Green
+B_Y='\033[1;33m'            # Yellow
+B_B='\033[1;34m'            # Blue
+B_P='\033[1;35m'            # Purple
+B_C='\033[1;36m'            # Cyan
+B_W='\033[1;37m'            # White
 
-# echo colors variables
+# Underline text color variables
+U_Black='\033[4;30m'        # Black
+U_R='\033[4;31m'            # Red
+U_G='\033[4;32m'            # Green
+U_Y='\033[4;33m'            # Yellow
+U_B='\033[4;34m'            # Blue
+U_P='\033[4;35m'            # Purple
+U_C='\033[4;36m'            # Cyan
+U_W='\033[4;37m'            # White
 
-cg="\033[32m" # Green
-cb="\033[34m" # Blue
-cr="\033[31m" # Red
-cy="\033[33m" # Yellow
-cv="\033[35m" # Violet
-cn="\033[0m"  # no color
+# Background color variables
+BG_Black='\033[40m'         # Black
+BG_R='\033[41m'             # Red
+BG_G='\033[42m'             # Green
+BG_Y='\033[43m'             # Yellow
+BG_B='\033[44m'             # Blue
+BG_P='\033[45m'             # Purple
+BG_C='\033[46m'             # Cyan
+BG_W='\033[47m'             # White
 
-# Short functions for color string
-echo_g(){ echo -e "$cg$1$cn"; }
+# Reset text color variable
+Rst='\033[0m'               # Reset
