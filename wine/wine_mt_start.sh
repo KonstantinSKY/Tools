7b4db47fb7b55e87e8a38fcc860c7674a3d56f16
SKY <sky012877@gmail.com>
2023-05-18 00:06:27 -0700

    wine/wine_mt_start.sh - Added :

---
+++ wine/wine_mt_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description :                                                            #
+############################################################################
+
+echo Starting Metatrader 5 in wine64...
+
+if ! $1
+
