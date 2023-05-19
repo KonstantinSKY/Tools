c2246e181eceffcf5825bbd722df7c0d48c67440
SKY <sky012877@gmail.com>
2023-05-18 17:09:32 -0700

    fstab_work.sh - Modified :

--- work_drive/fstab_work.sh
+++ work_drive/fstab_work.sh
@@ -3,28 +3,32 @@
 ############################################################################
 # Script name :                              Date   :                      #
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
-# Description :                                                            #
+# Description : Mounting Work disk                                         #
 ############################################################################
-dir=$HOME/Work
-mkdir -p $dir
+echo "Mounting a disk labelled "Work" onto a directory within the user's home directory named "Work" ..."
+
+dir="$HOME/Work"
+mkdir -p "$dir"
+
 echo Mounting Work disk to $dir
 echo Enter the sudo password:
-read -s password
-echo $password
+read -sp "" password
+
 drive=$(echo $password | sudo -S blkid | grep 'LABEL="Work"')
 echo  Drive :
 echo $drive
 
 drive_name=$(echo $drive | cut -f1 -d":")
-echo Anykey for add to etc/fstab next string
-str=$drive_name"                "$dir'   btrfs  noauto, x-systemd.automount              0 0'
-echo $str
-read anykey
-echo $str | sudo tee -a /etc/fstab
+
+echo "Press any key to add the following string to /etc/fstab"
+str="$drive_name                $dir   btrfs  noauto, x-systemd.automount              0 0"
+echo "$str"
+rread -n 1 -s anykey
+echo "$str" | sudo tee -a /etc/fstab
+
 echo Cheking fstab ...
 cat /etc/fstab
 
 findmnt --verify
 echo Done
-#echo Drive_data = $(echo $password | sudo -S blkid $drive_name)
 
