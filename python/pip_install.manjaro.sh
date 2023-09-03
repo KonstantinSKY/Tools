d2f4a4d3d42916bb5b633650288b6e116a9797bf
sky <sky012877@gmail.com>
2023-09-02 18:17:57 -0700

    pip_install.manjaro.sh - Modified :

--- python/pip_install.manjaro.sh
+++ python/pip_install.manjaro.sh
@@ -22,9 +22,25 @@ echo Upgrading pip...
 python -m pip install --upgrade pip
 python -m pip --version
 
+
+echo Anykey for install and setting pipx
+read anykey
+sudo pacman -S python-pipx
+
+
+echo Anykey for install and setting pipenv
+read anykey
+pipx install pipenv
+
+
 echo Anykey for install and setting pip-review
 read anykey
 echo installing pip-review ...
-python -m pip install pip-review
+pipx install pip-review
+
+
+echo Anykey for update python modules with pip-review
+read anykey
+
 pip-review --auto
 echo -e "Done! $cn"
