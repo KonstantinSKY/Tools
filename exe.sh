0561ab815d913b568d034407ff115f32c9911071
SKY <sky012877@gmail.com>
2024-02-15 11:01:08 -0800

    exe.sh - Modified :

--- exe.sh
+++ exe.sh
@@ -15,16 +15,40 @@ exe() {
     fi
 
 	echo -e "\033[35mNext Command: \033[34m\033[1m $1 \033[0m"
+	while true; do
+        echo -e "Press \033[36m\033[1mEnter\033[0m to execute the command, \033[36m\033[1mN \033[0m to skip, or \033[31m\033[1mQ\033[0m to quit the script: "
+        read -s -n 1 user_input
 
-	read -p "Press enter to execute the above command or N to skip: " user_input
+		# Move up two lines
+		echo -en "\033[2A"
+		# Clear the two lines
+		echo -e "\033[K"
+		echo -e "\033[K"
+		# Move up one more time to be at the beginning of the first cleared line
+    	echo -en "\033[1A"
 
-	if [[ $user_input == [Nn] ]]; then
-		 echo -e "Skipping: \033[34m\033[1m $1 \033[0m"
-	else
-		echo -e "\033[36mExecuting:"
-		echo -e	"\033[34m\033[1m$1\033[0m"
-		eval $1
-		 # Green color for finished message
-    	echo -e "\033[36mCommand finished\033[0m"
-	fi
+        case $user_input in
+            "")
+                echo -e "\033[36mExecuting:"
+                echo -e "\033[34m\033[1m$1\033[0m"
+                eval "$1"
+                # Green color for finished message
+                echo -e "\033[36mCommand finished\033[0m"
+                echo
+                break
+                ;;
+            [Nn])
+                echo -e "Skipping: \033[34m\033[1m $1 \033[0m"
+				echo
+                break
+                ;;
+            [Qq])
+                echo -e "\033[31mQuitting script.\033[0m"
+                exit 0
+                ;;
+            *)
+                echo -e "\033[31mInvalid input.\033[0m"
+                ;;
+        esac
+    done
 }
