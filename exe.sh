dc3ab8a7cc7d41cdf071d9238fb014d0acaa3db3
SKY <sky012877@gmail.com>
2024-03-06 23:27:32 -0800

    exe.sh - Modified :

--- exe.sh
+++ exe.sh
@@ -6,6 +6,79 @@
 # Description :                                                            #
 ############################################################################
 
+# Text color variables
+T_Black='\033[0;30m'        # Black
+T_R='\033[0;31m'            # Red
+T_G='\033[0;32m'            # Green
+T_Y='\033[0;33m'            # Yellow
+T_B='\033[0;34m'            # Blue
+T_P='\033[0;35m'            # Purple
+T_C='\033[0;36m'            # Cyan
+T_W='\033[0;37m'            # White
+
+# Bold text color variables
+B_Black='\033[1;30m'        # Black
+B_R='\033[1;31m'            # Red
+B_G='\033[1;32m'            # Green
+B_Y='\033[1;33m'            # Yellow
+B_B='\033[1;34m'            # Blue
+B_P='\033[1;35m'            # Purple
+B_C='\033[1;36m'            # Cyan
+B_W='\033[1;37m'            # White
+
+# Underline text color variables
+U_Black='\033[4;30m'        # Black
+U_R='\033[4;31m'            # Red
+U_G='\033[4;32m'            # Green
+U_Y='\033[4;33m'            # Yellow
+U_B='\033[4;34m'            # Blue
+U_P='\033[4;35m'            # Purple
+U_C='\033[4;36m'            # Cyan
+U_W='\033[4;37m'            # White
+
+# Background color variables
+BG_Black='\033[40m'         # Black
+BG_R='\033[41m'             # Red
+BG_G='\033[42m'             # Green
+BG_Y='\033[43m'             # Yellow
+BG_B='\033[44m'             # Blue
+BG_P='\033[45m'             # Purple
+BG_C='\033[46m'             # Cyan
+BG_W='\033[47m'             # White
+
+# Reset text color variable
+N_C='\033[0m'               # No Color
+
+
+start(){
+	echo -e "${T_Y}Script Started: ${B_P}$1 ${N_C}"
+	echo
+}
+
+end(){
+	echo -e "${T_Y}Script Finished: ${B_P}$1 ${N_C}"
+}
+
+show(){
+	echo -e "Showing file: ${B_W}$1${N_C} ..."
+	exe "cat $1"
+}
+
+copy(){
+	echo -e "Copying ${B_W}$2${N_C} ..."
+	exe "cp $1 $2 && ls -la $2"
+}
+
+commit(){
+	echo "Commiting to GitHub: $1 ..."
+	exe "git add *; git commit -a -m '$1'; git push"
+}
+
+py_add(){
+	echo "Add Python libraries: $1"
+	exe "poetry add $1"
+}
+
 # Define a function to execute a single-line command
 exe() {
     if [ -z "$1" ]; then
@@ -14,9 +87,9 @@ exe() {
         return 1
     fi
 
-	echo -e "\033[35mNext Command: \033[34m\033[1m $1 \033[0m"
+	echo -e "${T_P}Next Command: ${B_B}$1${N_C}"
 	while true; do
-        echo -e "Press \033[36m\033[1mEnter\033[0m to execute the command, \033[36m\033[1mN \033[0m to skip, or \033[31m\033[1mQ\033[0m to quit the script: "
+        echo -e "Press ${B_C}Enter${N_C} to execute the command, ${B_Y}N${N_C} to skip, or ${B_R}Q${N_C} to quit the script: "
         read -s -n 1 user_input
 
 		# Move up two lines
@@ -29,16 +102,17 @@ exe() {
 
         case $user_input in
             "")
-                echo -e "\033[36mExecuting:"
-                echo -e "\033[34m\033[1m$1\033[0m"
+                echo -e "${T_C}Executing command:"
+                echo -e "${B_B}$1${N_C}"
+				echo
                 eval "$1"
                 # Green color for finished message
-                echo -e "\033[36mCommand finished\033[0m"
+                echo -e "${T_C}Command finished.${N_C}"
                 echo
                 break
                 ;;
             [Nn])
-                echo -e "Skipping: \033[34m\033[1m $1 \033[0m"
+                echo -e "Skipping command: ${B_B}$1${N_C}"
 				echo
                 break
                 ;;
