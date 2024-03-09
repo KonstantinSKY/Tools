9edd4932bfdf9af7323f90674530e7a50b9df220
SKY <sky012877@gmail.com>
2024-03-09 12:35:18 -0800

    vms/create_vm.sh - Modified :

--- vms/create_vm.sh
+++ vms/create_vm.sh
@@ -6,18 +6,24 @@
 # Description : Create and setup virtual machine in Virtual box            #
 ############################################################################
 
-echo Creating virtual machine
 
-if [ ! $1 ];
-then
-	echo Add the virtual machine config as argument. Continue is imposible
-	exit
-fi
+source ~/Tools/exe.sh
+start $0
+h1 "CREATE VIRTUAL BOX Machine"
+#dir=pwd
 
-conf=$PWD/$1
-. $conf
-echo Config file is $conf
+#iso_file=${dir}/$
 
+iso_file="${vm_directory}/${vm_name}/${image_link##*/}"
+
+
+#message="No config file as argument"
+#exit_if_not $config_file
+#exit_if_not_file $config_file
+#conf=$PWD/$1
+#. $conf
+h2 "Config parameters"
+echo VM Directory: $vm_directory
 echo Virtual machine name: $vm_name
 echo OS Type: $os_type
 echo HDD size: $hdd_size
@@ -26,66 +32,75 @@ echo Video RAM: $videoram
 echo Graphic Controller: $graphcontr
 echo 3D Accelerator: $accel3D
 echo ISO File: $iso_file
+echo
 
-#vms_directory=~/VirtualBox VMs/
+message="No Virtual mashine name in config"
+exit_if_not $vm_name
+
+
+h2 "Creating and Checking Virtual Machine Directory"
+exe "mkdir -p $vm_directory && ls -la $vm_directory;"
+
+echo Setting Default Virtual Machine Directory to $vm_directory ...
+exe "VBoxManage setproperty machinefolder $vm_directory"
+
+echo Checking Vbox properties ...
+exe "VBoxManage list systemproperties"
+
 
 echo Checking virtual machine list...
-VBoxManage list vms
+exe "VBoxManage list vms"
 
-echo Checking iso file existence...
-if [ ! -f $iso_file ];
-	then 
-		echo iso file: $iso_file is not existing
-		echo Start imposible
-		exit
-fi 
+echo Creating Virtual Machine $vm_name ...
+exe "VBoxManage createvm --name $vm_name --register"
 
-read -p "Any key for continue to create VM : " any_key
+cd $vm_directory/$vm_name; ls -la
 
-echo Creating VM $vm_name ...
-VBoxManage createvm --name $vm_name --register
+mv_to_old $iso_file
+
+echo Downloading ISO file ...
+download_or_exit $image_link && ls -la $iso_file
+
+echo Checking ISO file  $iso_file existence...
+exit_if_not_file $iso_file
 
 echo Creating brige interface for net adapter...
-VBoxManage modifyvm $vm_name --nic1 bridged --bridgeadapter1 eno2 --nictype1 82540EM --cableconnected1 on
+exe "VBoxManage modifyvm $vm_name --nic1 bridged --bridgeadapter1 eno2 --nictype1 82540EM --cableconnected1 on"
 
 echo Choosing Virtual machine OS Type...
-VBoxManage modifyvm $vm_name --ostype $os_type
+exe "VBoxManage modifyvm $vm_name --ostype $os_type"
 
-echo Setup $memory of memory... 
-VBoxManage modifyvm $vm_name --memory $memory
+echo Setup $memory of memory...
+exe "VBoxManage modifyvm $vm_name --memory $memory"
 
 echo Setup video memory to $videoram
-VBoxManage modifyvm $vm_name --vram $videoram
+exe "VBoxManage modifyvm $vm_name --vram $videoram"
 
 echo Setup graphic controller to $graphcontr
-VBoxManage modifyvm $vm_name --graphicscontroller=$graphcontr
+exe "VBoxManage modifyvm $vm_name --graphicscontroller=$graphcontr"
+
 echo Setup 3D acceleration = $accel3D
-VBoxManage modifyvm $vm_name --accelerate3d=$accel3D
+exe "VBoxManage modifyvm $vm_name --accelerate3d=$accel3D"
 
-dir=`VBoxManage showvminfo $vm_name | grep "Config file"`
-dir="/${dir#*/}"
-dir="${dir%/*}/"
-echo $dir
-cd "$dir"
-hdd_file="$dir$vm_name.vdi"
-
+hdd_file="$vm_directory/$vm_name/$vm_name.vdi"
 
 echo Creating HDD $hdd_file with size = $hdd_size ...
-VBoxManage createhd --filename "$hdd_file" --size $hdd_size
-ls 
+exe "VBoxManage createhd --filename "$hdd_file" --size $hdd_size ;  ls -la"
 
 echo Adding IDE controller ...
-VBoxManage storagectl $vm_name --name "IDE Controller" --add ide
+exe "VBoxManage storagectl $vm_name --name 'IDE Controller' --add ide"
 
 echo Connecting HDD to IDE controller...
-VBoxManage storageattach $vm_name --storagectl "IDE Controller"  --port 0 --device 0 --type hdd --medium "$hdd_file"
-
+exe "VBoxManage storageattach $vm_name --storagectl 'IDE Controller'  --port 0 --device 0 --type hdd --medium $hdd_file && ls -la"
+pwd
 echo Connecting ISO file...
+exe "VBoxManage storageattach $vm_name --storagectl 'IDE Controller' --port 1 --device 0 --type dvddrive --medium $iso_file"
 
-VBoxManage storageattach $vm_name --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium $iso_file
+echo Showing VM information...
+exe "VBoxManage showvminfo $vm_name"
 
-
-VBoxManage showvminfo $vm_name 
-read -p "Any key for continue to START VM : " any_key
 echo Starting VM...
-VBoxManage startvm $vm_name
+exe "VBoxManage startvm $vm_name"
+
+end $0
+
