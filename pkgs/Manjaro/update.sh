e833b32988fd7b24b022a59129ad2cfd1515fe18
SKY <sky012877@gmail.com>
2024-03-14 17:38:44 -0700

    pkgs/Manjaro/update.sh - Modified :

--- pkgs/Manjaro/update.sh
+++ pkgs/Manjaro/update.sh
@@ -2,33 +2,32 @@
 
 
 source ~/Tools/exe.sh
-
 start $0
+h1 MANJARO PACKAGES UPDATING
 
-echo "System updating with pacman ..."
+h2 System updating with pacman
 exe "sudo pacman -Syu"
 
-echo "Checking system updating via pacman ..."
+h2 Checking system updating via pacman
 exe "pamac checkupdates -a"
 
 
-echo "Updating system via pacman ..."
+h2 Updating system via pacman
 exe "sudo pamac upgrade -a"
 
-echo "Checking for orphaned packages ..."
+h2 Checking for orphaned packages
 exe "pacman -Qdt; echo '---'; pamac list -o"
 
-echo "Removing all orphans ..."
+h2 Removing all orphans
 exe "sudo pamac remove -o"
 
-
-echo "Checking for orphaned packages ..."
+h2 Checking for orphaned packages
 exe "pacman -Qdt; echo '---'; pamac list -o"
 
-echo Removing all unused packages from cache...
+h2 Removing all unused packages from cache
 exe "sudo pacman -Sc"
 
-echo Removimg old packages from  more except for the latest three package versions...
+h2 Removing old packages from  more except for the latest three package versions
 exe "sudo paccache -rvk3"
 
 end $0
