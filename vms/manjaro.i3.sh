78dd9ef7659513e5431c84817ff9498042665303
SKY <sky012877@gmail.com>
2024-03-14 17:32:50 -0700

    vms/manjaro.i3.sh - Modified :

--- vms/manjaro.i3.sh
+++ vms/manjaro.i3.sh
@@ -3,7 +3,6 @@ echo Manjaro i3 virtual machine creator
 
 image_link="https://download.manjaro.org/i3/23.0.1/manjaro-i3-23.0.1-minimal-230921-linux65.iso"
 
-
 vm_directory="$HOME/Work/VirtualBox_VMs"
 
 vm_name=Manjaro_i3
@@ -15,5 +14,3 @@ graphcontr=vmsvga
 accel3D=on
 
 source create_vm.sh
-
-
