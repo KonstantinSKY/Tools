bb983450b25f80a52fe1888240b3a86e92618dae
SKY <sky012877@gmail.com>
2024-03-21 14:16:32 -0700

    enable_ssh.manjaro.sh - Modified :

--- ssh/enable_ssh.manjaro.sh
+++ ssh/enable_ssh.manjaro.sh
@@ -6,19 +6,26 @@
 # Description : Enable OpenSSH daemon for Manjaro                          #
 ############################################################################
 
-echo Checking how installed openssh...
-which ssh
-systemctl status sshd
 
-echo Starting and Enabling openssh daemon...
-sudo systemctl start sshd
-sudo systemctl enable sshd
-systemctl status sshd
+source ../exe.sh
 
-echo Showing IP v4...
-ip a | grep "inet "
+start $0
+h1 Enable openssh for Manjaro Linux
 
+h2 Checking openssh  status
+exe "which ssh && systemctl status sshd"
 
+h2 Starting sshd
+exe "sudo systemctl start sshd"
 
+h2 Enabling sshd
+exe "sudo systemctl enable sshd"
 
+h2 Checking openssh  status
+exe "systemctl status sshd"
+
+h2 Showing IP v4
+exe "ip a | grep 'inet '"
+
+end $0
 
