151070b67a1fcbb63210dafcc9493f973a512b41
SKY <sky012877@gmail.com>
2024-03-22 15:16:48 -0700

    docker/install.sh - Modified :

--- docker/install.sh
+++ docker/install.sh
@@ -8,29 +8,33 @@
 
 source ~/Tools/exe.sh
 start $0
-echo DOCKER INSTALLING
+h1 DOCKER INSTALL
 
-echo "Updating system packages..."
+h2 Updating system packages
 exe "sudo pacman -Syu"
 
-echo "Checking if Docker is already installed..."
+h2 "Checking if Docker is already installed..."
 docker --version
 docker-compose --version
 
-echo "Installing Docker & Docker-compose ..."
+h2 "Installing Docker & Docker-compose ..."
 exe "sudo pacman -Sy docker docker-compose --noconfirm"
 docker --version
 docker-compose --version
 
 
-echo Starting Docker systemclt ...
+h2 Starting Docker systemclt ...
 exe "sudo systemctl start docker"
 exe "sudo systemctl enable docker"
 exe "sudo systemctl status docker --no-pager"
 
 
-echo Adding current user to docker group ...
+h2 Adding current user to docker group ...
 exe "sudo usermod -aG docker $USER"
+
+h2  Installing docker-buildx
+exe "sudo pacman -Sy docker-buildx"
+
 
 echo "Checking Docker by Hello world (test Image) ..."
 exe "docker run hello-world"
