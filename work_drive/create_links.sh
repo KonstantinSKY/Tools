9a86c119e5f5e4579428d656d2a5e59b84472e6b
SKY <sky012877@gmail.com>
2024-03-31 23:31:41 -0700

    work_drive/create_links.sh - Modified :

--- work_drive/create_links.sh
+++ work_drive/create_links.sh
@@ -9,54 +9,49 @@
 directories="Tools Downloads Security Documents .password-store Music Pictures Projects Videos Configs Obsidian"
 
 echo Creating symbolic links from ~/ to ~/Work ...
-echo $directories
+echo "$directories"
 
-for dir in $directories
-do
+for dir in $directories; do
 	echo ---------------------------------------------
 	target_dir=$HOME/Work/$dir
 	dir=$HOME/$dir
 
-	echo Checkng $dir for link:
-	if [ -L "$dir" ] ; then
-   		if [ -e "$dir" ] ;
-		then
-      		echo "Link exist and good"
-			echo "$dir --> $(readlink -f $dir)"
-			continue
-   		else
-    		echo "Broken link will be remove"
-	  		rm $dir
-   		fi
-	fi
-
-	echo Checkng $dir for directory:
-	if [ -d "$dir" ];
-	then
-		if [ "$(ls -A $dir)" ] ; then
-     		echo "$dir is not Empty"
+	echo Checkng "$dir" for link:
+	if [ -L "$dir" ]; then
+		if [ -e "$dir" ]; then
+			echo "Link exist and good"
+			echo "$dir --> $(readlink -f "$dir")"
 			continue
 		else
-    		echo "$dir is Empty and will be remove"
-			rm -r $dir
+			echo "Broken link will be remove"
+			rm "$dir"
 		fi
 	fi
 
-	echo Checkng $target_dir for directories:
-	if [ -d "$target_dir" ] ; then
-		echo $target_dir is exist
+	echo Checkng "$dir" for directory:
+	if [ -d "$dir" ]; then
+		if [ "$(ls -A "$dir")" ]; then
+			echo "$dir is not Empty"
+			continue
+		else
+			echo "$dir is Empty and will be remove"
+			rm -r "$dir"
+		fi
+	fi
+
+	echo Checkng "$target_dir" for directories:
+	if [ -d "$target_dir" ]; then
+		echo "$target_dir" is exist
 	else
-		echo $target_dir is not exist will be created
-		mkdir -p $target_dir
+		echo "$target_dir" is not exist will be created
+		mkdir -p "$target_dir"
 	fi
 
 	echo Creating symbolic link:
-	echo $dir "-->" $target_dir
-	ln -s $target_dir $dir
+	echo "$dir" "-->" "$target_dir"
+	ln -s "$target_dir" "$dir"
 
 	echo Checking by readlink...
-	readlink -f $dir
+	readlink -f "$dir"
 
 done
-
-
