dcf6c88f17dd18a4c0b0a5d37c9a69d17a11fee5
SKY <sky012877@gmail.com>
2024-03-31 23:31:41 -0700

    work_drive/mount_work_disk.sh - Modified :

--- work_drive/mount_work_disk.sh
+++ work_drive/mount_work_disk.sh
@@ -5,8 +5,8 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description : Mounting Work disk                                         #
 ############################################################################
-source ../exe.sh
-start $0
+source "$EXE"
+start "$0"
 h1 "Mount Work disk"
 
 label="Work"
@@ -21,38 +21,34 @@ h1 Mounting Work Drive
 h2 Find the device associated with label $label
 exe "sudo findfs LABEL='$label'" --result
 drive=$result
-message="Drive with label: $label not found"
-exit_if_not $drive
+exit_if_not "$drive" "Drive with label: $label not found"
 echo "Drive with label: $label was found: $drive"
 
-
-h2 Creating work directory: $dir
+h2 Creating work directory: "$dir"
 exe "mkdir -p $dir; ls -la $dir"
 
-h2 Mounting $drive to $dir
+h2 Mounting "$drive" to "$dir"
 exe "sudo mount '$drive' '$dir'"
 
-h2 Checking mounted drive $drive
+h2 Checking mounted drive "$drive"
 exe "mount | grep $dir"
 
-h2 Setting full access for user:  $USER
+h2 Setting full access for user: "$USER"
 exe "sudo chown -R $USER:$USER $dir && sudo chmod -R 700 $dir && ls -la $dir"
-
 
 h1 Change fstab file for automounting
 
-h2 Retrieving the UUID of the drive: $drive
+h2 Retrieving the UUID of the drive: "$drive"
 exe "sudo blkid -s UUID -o value '$drive'" --result
 UUID=$result
 echo "UUID: $UUID"
 
-message="Unable to find UUID for $drive. Ensure the device is connected and try again."
-exit_if_not $UUID
+exit_if_not "$UUID" "Unable to find UUID for $drive. Ensure the device is connected and try again."
 
 # Check if the entry already exists in /etc/fstab
 if grep -qs "UUID=$UUID" /etc/fstab; then
     echo "An entry for this UUID ($uuid) already exists in /etc/fstab."
-	end $0 
+    end "$0"
 fi
 
 h2 Backup the current fstab
@@ -67,5 +63,4 @@ exe "cat /etc/fstab"
 h2 System rebooting
 exe "sudo reboot"
 
-end $0
-
+end "$0"
