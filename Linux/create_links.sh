0a786a2c1606c28d5b95b686b842a76ff8102186
SKY <sky012877@gmail.com>
2024-04-05 23:12:23 -0700

    Linux/create_links.sh - Modified :

--- Linux/create_links.sh
+++ Linux/create_links.sh
@@ -5,7 +5,7 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description : Create symbolic link to Work disk                          #
 ############################################################################
-source .exe
+source $HOME/.exe
 
 h1 Directory Symbolic Links Creator Script
 
