c3659eaf2888174019d024d5de63d9acd30e64cd
SKY <sky012877@gmail.com>
2024-04-06 11:44:40 -0700

    Linux/run_loader.sh - Modified :

--- Linux/run_loader.sh
+++ Linux/run_loader.sh
@@ -8,28 +8,27 @@ echo Creating RUN directory...
 mkdir -p RUN
 echo ---
 
-echo Getting exe.sh
+echo Getting exe.sh  shell script helper module
 wget -O RUN/exe.sh https://raw.githubusercontent.com/KonstantinSKY/Tools/main/exe.sh
-echo ---
-
-echo Getting run.sh ...
-wget -O RUN/run.sh https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/run.sh
-echo ---
-
-echo Getting mount_work_disk.sh
-wget -O RUN/mount_work_disk https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/mount_work_disk.sh
-scho ---
-
-ls -la RUN
-echo ---
-
-echo  Deleting run_loader.sh ...
-rm -f run_loader.sh; ls run_loader.sh
 echo ---
 
 source RUN/exe.sh
 
+h2 Getting run.sh
+exe "wget -O RUN/run.sh https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/run.sh" -n
+
+h2 Getting mount_work_disk.sh
+exe "wget -O RUN/mount_work_disk https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/mount_work_disk.sh" -n
+
+h2 Showing RUN directory 
+exe "ls -la RUN"
+
+h2 Removing run_loader.sh ...
+exe "rm -f run_loader.sh; ls run_loader.sh"
+
+
 h1 LINUX SETUP RUN
 exe "bash RUN/run.sh" 
+
 
 end
