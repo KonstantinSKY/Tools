51549c786785ae4e8371a6955df7c4b8dd0c22dd
SKY <sky012877@gmail.com>
2024-04-06 16:46:56 -0700

    Linux/Manjaro_i3/setup.sh - Added :

---
+++ Linux/Manjaro_i3/setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+############################################################################
+# Script name : first_setup.sh               Date   : 10/02/22             #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Add env vars and my rc part to bashrc and to others ..rs fi#
+############################################################################
+
+source "$HOME"/.exe
+
+h1 MANJARO I3 FULL SETUP
+
+h2 Creating links to config files
+
+h2 Setting System Time
+
+h2 Updating Repozitory Mirrors
+
+h2 Updating Packages
+
+h2 Checking Kernel
+
+h2 Updating Packages
+
+# Install and Setup Application 
+h2 Trash
+
+h2 Google Chrome
+
+h2 Megasync
+
+h2 Security and Pass
+
+h2 SSH
+
+h2 Git
+
+h2 Fonts
+
+h2 Python
+
+h2 JavaScript
+
+
+
+h2 Installing Packages
+
+h2 Installing Others 
+
+
+end
