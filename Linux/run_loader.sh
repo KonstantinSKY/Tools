dd1b4b8576341a5a9822e14fd5b65da98c9988c8
SKY <sky012877@gmail.com>
2024-04-06 11:02:44 -0700

    Linux/run_loader.sh - Modified :

--- Linux/run_loader.sh
+++ Linux/run_loader.sh
@@ -7,11 +7,23 @@ echo LOADER for run.sh and additional packages for Linux Setting
 echo Creating RUN directory...
 mkdir -p RUN
 
-echo Getting run.sh ...
-wget -p RUN/ https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/run_loader.sh
-
 echo Getting exe.sh
 wget -p RUN/ https://raw.githubusercontent.com/KonstantinSKY/Tools/main/exe.sh
 
+echo Getting run.sh ...
+wget -p RUN/ https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/run_loader.sh
+
 echo Getting mount_work_disk.sh
-wget -p RUN/ https://raw.githubusercontent.com/KonstantinSKY/Tools/main/exe.sh
+wget -p RUN/ https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/mount_work_disk.sh
+
+ls -la RUN
+
+echo  Deleting run_loader.sh ...
+rm -f run_loader.sh
+
+source RUN/exe.sh
+
+h1 LINUX SETUP RUN
+exe "bash RUN/run.sh" 
+
+end
