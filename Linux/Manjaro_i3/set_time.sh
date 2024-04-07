9e12184f225f55bcf551a9e5e828c611fb01898d
SKY <sky012877@gmail.com>
2024-04-06 22:59:29 -0700

    Linux/Manjaro_i3/set_time.sh - Modified :

--- Linux/Manjaro_i3/set_time.sh
+++ Linux/Manjaro_i3/set_time.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-############################################################################
-# Script name :                              Date   :                      #
-# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
-# Description :                                                            #
-############################################################################
-
 echo Setting system clock auto sync...
 sudo timedatectl set-ntp true
 timedatectl status
