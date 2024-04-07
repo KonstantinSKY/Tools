7a94c014a007c189756bed076b65db51508f1cbf
SKY <sky012877@gmail.com>
2024-04-07 13:24:01 -0700

    packages/Manjaro/mirrors.sh - Added :

---
+++ packages/Manjaro/mirrors.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+############################################################################
+# Script name :                              Date   :                      #
+# Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
+# Description : Setup repo mirrors for packages                            #
+############################################################################
+
+# shellcheck disable=SC1091
+source "$HOME"/.exe
+
+h1 REPO MIRRORS UPDATE
+
+h2 Showing mirrors status
+exe "pacman-mirrors --status" --noconfirm
+
+h2 Searching and updating fastests
+exe "sudo pacman-mirrors --fasttrack 10"
+
+h2 Showing New status of mirrors pool ...
+exe "pacman-mirrors --status"
+
+end 
