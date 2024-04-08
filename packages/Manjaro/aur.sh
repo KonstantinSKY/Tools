c7a47bc50631be529c094535952858101ef8ce7f
SKY <sky012877@gmail.com>
2024-04-08 11:57:05 -0700

    packages/Manjaro/aur.sh - Modified :

--- packages/Manjaro/aur.sh
+++ packages/Manjaro/aur.sh
@@ -12,57 +12,13 @@ source "$HOME"/.exe
 #config files
 CONFIG=/etc/pamac.conf
 
-#h1 "PAMAC & AUR (ADVANCED USER REPOSITORY) SETUP in $CONFIG"
+h1 "PAMAC & AUR (ADVANCED USER REPOSITORY) SETUP in $CONFIG"
+exit_if_not_file "$CONFIG"
+show $CONFIG
+backup $CONFIG --sudo
 
-#show $CONFIG
-
-#backup $CONFIG --sudo
-
-#enable_parameter "EnableAUR" $CONFIG "AUR"  --sudo
-
-
-
-# h2 Enabling AUR
-# exe "sudo sed -i 's/#EnableAUR/EnableAUR/' $CONFIG"
-# echo Checking if the EnableAUR line is uncommented, if not, adding it
-# if ! grep -q "^EnableAUR" $CONFIG; then
-#     echo "EnableAUR" | sudo tee -a $CONFIG >/dev/null
-# fi
-# #
-
-# h2 Enabling Check AUR Updates
-# exe "sudo sed -i 's/#CheckAURUpdates/CheckAURUpdates/' $CONFIG"
-# echo Checking if the CheckAURUpdates line is uncommented, if not, adding it
-# if ! grep -q "^CheckAURUpdates" $CONFIG; then
-#     echo "CheckAURUpdates" | sudo tee -a "$CONFIG" >/dev/null
-# fi
-
-# h2 Enabling RemoveUnrequiredDeps
-# exe "sudo sed -i 's/#RemoveUnrequiredDeps/RemoveUnrequiredDeps/' $CONFIG"
-# echo Checking if the CheckAURUpdates line is uncommented, if not, adding it
-# if ! grep -q "^RemoveUnrequiredDeps" $CONFIG; then
-#     echo "RemoveUnrequiredDeps" | sudo tee -a "$CONFIG" >/dev/null
-# fi
-
-# h2 Enabling NoUpdateHideIcon
-# exe "sudo sed -i 's/#NoUpdateHideIcon/NoUpdateHideIcon/' $CONFIG"
-# echo Checking if the CheckAURUpdates line is uncommented, if not, adding it
-# if ! grep -q "^NoUpdateHideIcon" $CONFIG; then
-#     echo "NoUpdateHideIcon" | sudo tee -a "$CONFIG" >/dev/null
-# fi
-
-# h2 Enabling NoUpdateHideIcon
-# exe "sudo sed -i 's/#NoUpdateHideIcon/NoUpdateHideIcon/' $CONFIG"
-
-# Checking_param() {
-#     local param=$1
-#     echo Checking if the "$param" line is uncommented, if not, adding it
-#     if ! grep -q "^$param" $CONFIG; then
-#         echo "$param" | sudo tee -a "$CONFIG" >/dev/null
-#     fi
-# }
-
-# show Cheking pamac config for AUR ...
-# exe "cat $CONFIG"
-
-# end
+enable_parameter "EnableAUR" $CONFIG "Allow Pamac to search and install packages from AUR"  --sudo
+enable_parameter "CheckAURUpdate" $CONFIG "When AUR support is enabled check for updates from AUR"  --sudo
+enable_parameter "RemoveUnrequiredDeps" $CONFIG "When removing a package, also remove those dependencies that are not required by other packages (recurse option)"  --sudo
+enable_parameter "NoUpdateHideIcon" $CONFIG "When no update is available, hide the tray icon"  --sudo
+enable_parameter "DownloadUpdates" $CONFIG "Download updates in background"  --sudo
