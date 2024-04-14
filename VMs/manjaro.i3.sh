5fd9c1ef5652ce23161e8b04e68e7a0852cf13ce
SKY <sky012877@gmail.com>
2024-04-14 10:52:05 -0700

    VMs/manjaro.i3.sh - Added :

---
+++ VMs/manjaro.i3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo Manjaro i3 virtual machine creator
+
+image_link="https://download.manjaro.org/i3/23.0.1/manjaro-i3-23.0.1-minimal-230921-linux65.iso"
+
+vm_directory="$HOME/Work/VirtualBox_VMs"
+
+vm_name=Manjaro_i3
+os_type=Linux_64
+hdd_size=50000
+memory=8192
+videoram=128
+graphcontr=vmsvga
+accel3D=on
+
+source create_vm.sh
