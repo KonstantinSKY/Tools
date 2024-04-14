2846082f75ab15c64d050896d9499f0d55bee754
SKY <sky012877@gmail.com>
2024-04-13 21:52:16 -0700

    ../../git/commit.sh - Modified :

--- git/commit.sh
+++ git/commit.sh
@@ -1,52 +1,61 @@
 #!/bin/bash
 
-. ~/Tools/exe.sh
+# shellcheck disable=SC1091
+. "$HOME"/.exe
+
+force_flag=""
+for param in "${@}"; do
+	# shellcheck disable=SC1073
+	if [ "$param" = "-f" ] || [ "$param" = "--force" ]; then
+		force_flag="-f"
+	fi
+done
 
 h1 GIT FULL COMMIT
 
-echo -e "$T_C Git status : $N_C"
-exe "git status" --noconfirm
+git_status=$(git status -s)
+if [ -z "$git_status" ]; then
+	echo -e "{T_C}Nothing to do${N_C}"
+	exit
+fi
 
-[ ! `git status -s` ] && exit || git status -s
+echo -e "$T_C Git status: $N_C"
+exe "git status" --noconfirm
+# echo "$git_status"
 
 echo -e "$T_C Adding all files ... $N_C"
 exe "git add -v *"
 
-echo -e "$T_W Change analizing ... $N_C"
-exe "git status -s"
-
-str_arr=(`git status -s`)
-for idx in ${!str_arr[@]}
-do
-	[ $(($idx % 2)) -eq 0 ] && f_type=${str_arr[$idx]} || continue
-	file=${str_arr[$idx+1]}
+git status -s | while IFS=' ' read -r f_type file; do
+	echo "status $f_type"
+	echo "File $file"
 	case $f_type in
-		M)
-			message="Modified" ;;
-		AM)
-			message="Added and modified" ;;
-		A)
-			message="Added" ;;
-		D)
-			message="Deleted"
-#			[ -f $file ] && git rm -r $file || echo $file not found at the file system.
-			;;
-		*)
-			echo "Unkown type: $ftype"
-			continue
+	M) message="Modified" ;;
+	AM) message="Added and modified" ;;
+	A) message="Added" ;;
+	D) message="Deleted" ;;
+	*)
+		echo "Unkown type: $f_type"
+		continue
 		;;
 	esac
 
-	echo -e "$cb ======================================================== $cn"
-	echo -e "$cr $file - $message :$cn"
-	echo -e Commiting for $B_P$file$N_C:
-	echo -e "$T_Y * Text your message for the commit *$N_C"
-	read -e -p " " -i "$file - $message : " u_message
-	echo -e "$B_C Commiting ...$N_C"
-#	echo $u_message
-	git commit -m "$u_message" $file
-done
+	echo -e "$ ========================================================"
+	message="$file - $message :"
 
+	echo -e "$message"
+	echo -e "Commiting for ${B_P}$file${N_C}":
+	if [ -z "$force_flag" ]; then
+		echo -e "${T_Y} * Text your message for the commit *{$N_C}"
+		read -r -e -p " " -i "$file - $message : " u_message
+	else
+		u_message=$message
+	fi
+
+	echo -e "$B_C Commiting ...$N_C"
+	#	echo $u_message
+	git commit -m "$u_message" "$file"
+done
 echo -e "$T_C Git status: $N_C"
 exe "git status"
 
@@ -54,7 +63,6 @@ echo -e "$B_C Pushing ...$N_C"
 exe "git push"
 
 echo -e "$T_C Git status: $T_C"
-exe "git status"  --noconfirm
+exe "git status" --noconfirm
 
-
-end $0
+end "$0"
