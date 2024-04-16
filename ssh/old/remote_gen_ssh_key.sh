45b67be80185e5ed744f52f7ad9dd5e231d52662
SKY <sky012877@gmail.com>
2024-04-15 23:27:08 -0700

    ssh/old/remote_gen_ssh_key.sh - Added :

---
+++ ssh/old/remote_gen_ssh_key.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+
+key_file="~/.ssh/id_ed25519"
+
+echo Create ssh key at a remote host
+
+echo Preparing for creating ssh key ...
+
+. get_hostname.sh
+
+echo Hostname: $hostname
+
+echo Key file is $key_file
+echo Enter email for key file:
+read email
+echo push any key to create the keyfile: $key_file at $hostname with email: $email
+read anykey
+echo Connecting to remote host: $hostname
+kysky
+ssh $hostname << EOF
+
+echo We are inside the remote host: $hostname
+echo Generating SSH keys....
+
+ssh-keygen -t ed25519 -f $key_file -C $email -N ""
+
+
+echo $key_file public key:
+
+cat $key_file.pub
+EOF
+
+echo Returned to the local host!
+echo Done!
