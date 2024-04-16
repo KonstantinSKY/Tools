d6a1dea70f8d1d7b16d68dd08392853153587952
SKY <sky012877@gmail.com>
2024-04-15 23:24:28 -0700

    ../../pass/install.manjaro.sh - Modified :

--- pass/install.manjaro.sh
+++ pass/install.manjaro.sh
@@ -6,26 +6,40 @@
 # Description : Setup password manager for manjaro                         #
 ############################################################################
 
-. $HOME/.exe
+. "$EXE"
 
 packages=$TOOLS_PATH/packages/Manjaro
+
+#Directories
+link_store="$HOME"/.password-store
+source_store="$HOME"/Work/.password-store
+
 #scripts
-import_keys=$HOME/Work/Security/import_gpg_keys.sh   
+import_keys=$HOME/Work/Security/import_gpg_keys.sh
 
-H1 pass Installation and setting
+h1 pass Installation and setting
 
-h2 Installing GnuPG  application
-exe "bash $packages/install gnupg"
+h2 Installing GnuPG application
+exe "bash $packages/install.sh gnupg"
 
-
-h2 Installing GnuPG  application
-exe "bash $packages/install pass && pass version"
+h2 Installing GnuPG application
+exe "bash $packages/install.sh pass && pass version"
 
 h2 Importing Keys
-exe  "bash $import_keys"
+exe "bash $import_keys"
 
-h2 List GnuPG Privat and Public Keys; 
+h2 List GnuPG Privat and Public Keys
 exe "gpg --list-keys; gpg --list-secret-keys; gpg --check-trustdb"
 
+h2 Creating store directory: "$source_store"
+exe "mkdir -p $source_store && ls -la $source_store"
 
+h2 Cloning .password-store
+exe "git clone git@gitlab.com:sky012877/Password-store.git $source_store"
 
+slink "$source_store" "$link_store"
+
+h2 Checking pass util
+exe "pass"
+
+end
