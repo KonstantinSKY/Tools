c2b15638c30b30382f81047f3e44f77d9a54eb88
SKY <sky012877@gmail.com>
2024-04-15 23:27:08 -0700

    ssh/setup.sh - Added :

---
+++ ssh/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+h1  ssh setup 
+
+slink "$HOME"/Work/Security/ssh "$HOME"/.ssh
+
+h2 Checkind GitHub ssh access
+exe "ssh -T git@github.com" -n
+
+h2 Checkind GitLab ssh access
+exe "ssh -T git@gitlab.com" -n
