d6f53ac720b8bad45fc363bdc7cc38b20688e471
SKY <sky012877@gmail.com>
2024-04-15 23:27:08 -0700

    ssh/old/copy_ssh_key.sh - Added :

---
+++ ssh/old/copy_ssh_key.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Copy ssh key to remote PC:
+. get_hostname.sh
+
+read -p "Anykey if You wanna copy ssh-key to:  $hostname" anykey
+
+echo Copying ssh key to $hostname ...
+ssh-copy-id $hostname
+
+echo Trying to connect to $hostname ...
+ssh $hostname
+
