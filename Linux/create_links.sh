9d0783356e9ec08eb62d2f193809929a27f2b0c3
SKY <sky012877@gmail.com>
2024-04-17 10:05:58 -0700

    Linux/create_links.sh - Modified :

--- Linux/create_links.sh
+++ Linux/create_links.sh
@@ -6,9 +6,12 @@
 # Description : Create symbolic link to Work disk                          #
 ############################################################################
 # shellcheck disable=SC1091
-source "$HOME"/.exe
+# source "$HOME"/.exe
 
-h1 Directory Symbolic Links Creator Script
+. exe
+
+
+h1 Directory Symbolic Links Creator
 
 directories="Tools Downloads Security Documents Music Pictures Projects Videos Configs Obsidian BackUps VirtualBox_VMs"
 
@@ -16,46 +19,50 @@ h2 Symbolic links from ~/ to ~/Work will be created for next Directories:
 h2 "$directories"
 
 for dir in $directories; do
-	echo ---------------------------------------------
-	target_dir=$HOME/Work/$dir
-	dir=$HOME/$dir
-	h1 "Symbolic link $dir --> $target_dir"
+	mkdir -p "$HOME"/Work/"$dir"
+	slink "$HOME/Work/$dir" "$HOME/$dir" "$dir"
 
-	h2 Checking "$dir" for link existing:
-	if [ -L "$dir" ]; then
-		if [ -e "$dir" ]; then
-			echo "Link exist and is good"
-			echo "$dir --> $(readlink -f "$dir")"
-			continue
-		else
-			echo -e "${T_R}" "Broken link $dir will be remove""${N_C}"
-			exe "rm ""$dir"""
-		fi
-	fi
+	# echo ---------------------------------------------
 
-	h2 Checking source "$dir" for directory existing:
-	if [ -d "$dir" ]; then
-		if [ "$(ls -A "$dir")" ]; then
-			echo -e "${T_R}" "$dir is not Empty, skiping""${N_C}"
-			continue
-		else
-			echo "$dir is Empty and will be remove"
-			h2 Removing directory "$dir"
-			exe "rm -r $dir"
-		fi
-	fi
+	# target_dir=$HOME/Work/$dir
+	# dir=$HOME/$dir
+	# h1 "Symbolic link $dir --> $target_dir"
 
-	h2 Checking target "$target_dir" for directories:
-	if [ -d "$target_dir" ]; then
-		echo "$target_dir" is exist
-	else
-		echo "$target_dir" is not exist will be created
-		h2 Creating directory "$target_dir"
-		exe "mkdir -p $target_dir; ls $target_dir" -n
-	fi
+	# h2 Checking "$dir" for link existing:
+	# if [ -L "$dir" ]; then
+	# 	if [ -e "$dir" ]; then
+	# 		echo "Link exist and is good"
+	# 		echo "$dir --> $(readlink -f "$dir")"
+	# 		continue
+	# 	else
+	# 		echo -e "${T_R}" "Broken link $dir will be remove""${N_C}"
+	# 		exe "rm ""$dir"""
+	# 	fi
+	# fi
 
-	h1 "Creating and checking symbolic link: $dir -->  $target_dir"
-	exe "ln -sv $target_dir $dir; readlink -f $dir"
+	# h2 Checking source "$dir" for directory existing:
+	# if [ -d "$dir" ]; then
+	# 	if [ "$(ls -A "$dir")" ]; then
+	# 		echo -e "${T_R}" "$dir is not Empty, skiping""${N_C}"
+	# 		continue
+	# 	else
+	# 		echo "$dir is Empty and will be remove"
+	# 		h2 Removing directory "$dir"
+	# 		exe "rm -r $dir"
+	# 	fi
+	# fi
+
+	# h2 Checking target "$target_dir" for directories:
+	# if [ -d "$target_dir" ]; then
+	# 	echo "$target_dir" is exist
+	# else
+	# 	echo "$target_dir" is not exist will be created
+	# 	h2 Creating directory "$target_dir"
+	# 	exe "mkdir -p $target_dir; ls $target_dir" -n
+	# fi
+
+	# h1 "Creating and checking symbolic link: $dir -->  $target_dir"
+	# exe "ln -sv $target_dir $dir; readlink -f $dir"
 
 done
 end
