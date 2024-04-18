85337bc500e5d7e53f87e084fdab5b5798c30f56
SKY <sky012877@gmail.com>
2024-04-18 16:17:49 -0700

    Linux/loader.sh - Modified :

--- Linux/loader.sh
+++ Linux/loader.sh
@@ -28,18 +28,15 @@ wget -O "$EXE_LIB" https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Li
 . exe.lib
 
 h2 Removing loader.sh
-exe "rm -fv run_loader.sh; ls run_loader.sh" -n
-
-h2 Getting mount_work_disk.sh
-exe "wget -O $MOUNT_SCR https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/mount_work_disk.sh"
+exe "rm -f run_loader.sh; ls run_loader.sh" -n
 
 h1 Linux after Instalation Setting
 
 h2 Creating Work directory if not exists
 exe "mkdir -pv $WORK_DIR; ls -la $WORK_DIR"
 
-h2 Starting mounting Work disk to Work Directory Script
-exe "bash $MOUNT_SCR"
+h2 "Getting mount_work_disk.sh and Starting Mounting script $MOUNT_SCR" 
+exe "wget -O $MOUNT_SCR https://raw.githubusercontent.com/KonstantinSKY/Tools/main/Linux/mount_work_disk.sh && bash $MOUNT_SCR"
 
 h1 Cloning Repos
 h2 Cloning CONFIGS repository to "$CONFIGS_DIR"
