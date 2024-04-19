bf979593891ed5f52436aae0c72253ae81308958
SKY <sky012877@gmail.com>
2024-04-19 14:21:15 -0700

    Linux/Manjaro_i3/dev_setup.sh - Modified :

--- Linux/Manjaro_i3/dev_setup.sh
+++ Linux/Manjaro_i3/dev_setup.sh
@@ -4,17 +4,18 @@
 
 #scripts
 docker=$TOOLS_PATH/docker/install.sh
-
+vscode=$TOOLS_PATH/vscode/install.manjaro.sh
 h1 DEVELOPER SETUP
 
 h1 Docker
 run $docker "Docker Installation  and first setting" 
 
 h1 VSCODE
+run $vscode "VS Code editor installation"
 
 h1 PYCHARM
 
-h1 PYHON
+h1 PYTHON
 
 h1 JavaScript
 
