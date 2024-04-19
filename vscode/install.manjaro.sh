61d31231fc64fe0521a3b59188ed9ed57ee3e276
SKY <sky012877@gmail.com>
2024-04-19 14:21:15 -0700

    vscode/install.manjaro.sh - Added :

---
+++ vscode/install.manjaro.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. exe.lib
+
+#Directories
+target_dir="$HOME/.config/'Code - OSS'/User"
+
+#files
+config_source="$CONFIGS_PATH/vscode.settings.json"
+config_target="$target_dir/settings.json"
+
+h1 Visual STudio Code install and setting
+
+h2 Installing vscode
+add code
+
+slink  $config_source $config_target "Main Vscode config"
