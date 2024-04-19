f79afff6df55d802d9b6d0756e578da0ba62c6df
SKY <sky012877@gmail.com>
2024-04-18 23:13:36 -0700

    ../../docker/install.sh - Modified :

--- docker/install.sh
+++ docker/install.sh
@@ -5,40 +5,42 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description :                                                            #
 ############################################################################
+. exe.lib
 
-source ~/Tools/exe.sh
-start $0
-h1 DOCKER INSTALL
+#script
+install=$TOOLS_PATH/packges/install.sh
 
-h2 Updating system packages
-exe "sudo pacman -Syu"
+
+
+h1 DOCKER SETUP
 
 h2 "Checking if Docker is already installed..."
 docker --version
 docker-compose --version
 
 h2 "Installing Docker & Docker-compose ..."
-exe "sudo pacman -Sy docker docker-compose --noconfirm"
+install docker docker-compose docker-build
+
 docker --version
 docker-compose --version
 
-
 h2 Starting Docker systemclt ...
-exe "sudo systemctl start docker"
-exe "sudo systemctl enable docker"
-exe "sudo systemctl status docker --no-pager"
+exe "sudo systemctl start docker; sudo systemctl enable docker"
+
+h2 Show Docker Status
+exe "systemctl status docker --no-pager" -n
 
 
 h2 Adding current user to docker group ...
 exe "sudo usermod -aG docker $USER"
 
-h2  Installing docker-buildx
-exe "sudo pacman -Sy docker-buildx"
+# h2  Installing docker-buildx
+# exe "sudo pacman -Sy docker-buildx"
 
-h2 Installing docker-desktop
-exe "sudo pamac install docker-desktop"
+# h2 Installing docker-desktop
+# exe "sudo pamac install docker-desktop"
 
 echo "Checking Docker by Hello world (test Image) ..."
 exe "docker run hello-world"
 
-end $0
+end
