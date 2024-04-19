8732c58f806c65c85e27f78a254baee4086dc538
SKY <sky012877@gmail.com>
2024-04-19 11:36:14 -0700

    nvim/install.manjaro.sh - Modified :

--- nvim/install.manjaro.sh
+++ nvim/install.manjaro.sh
@@ -2,12 +2,18 @@
 
 . exe.lib
 
+#Directories
+CONFIG_DIR=$HOME/.config/nvim/
+
 h1 "NEOVIM Installation and setup for Manjaro Linux"
 
 h2 "Installing Neovim and Eco system..."
 add neovim neovim-plug
 
-slink "$CONFIGS_PATH/init.vim" "$HOME/.config/nvim/init.vim" "Init.vim config"
+h2 Making directory "$CONFIG_DIR"
+exe "mkdir -pv $CONFIG_DIR"
+
+slink "$CONFIGS_PATH/init.vim" "$config/init.vim" "Init.vim config"
 
 h2 install plugins
 exe "nvim -c 'PlugInstall' -c ':x' -c ':x'"
