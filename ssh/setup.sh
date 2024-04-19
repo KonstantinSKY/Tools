d707fe859207e4d10a5c4a7a209d7235da78858f
SKY <sky012877@gmail.com>
2024-04-18 19:55:48 -0700

    ../ssh/setup.sh - Modified :

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+. exe.lib
 
-h1  ssh setup 
+h1 ssh setup 
 
-slink "$HOME"/Work/Security/ssh "$HOME"/.ssh
+slink "$HOME/Work/Security/ssh" "$HOME/.ssh" "to Security Megasync directory" 
 
 h2 Checkind GitHub ssh access
 exe "ssh -T git@github.com" -n
