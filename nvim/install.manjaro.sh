ab394e2d63a3b7155a068e8a5ed5eb76115ddc73
SKY <sky012877@gmail.com>
2024-04-19 12:13:13 -0700

    nvim/install.manjaro.sh - Modified :

--- nvim/install.manjaro.sh
+++ nvim/install.manjaro.sh
@@ -17,21 +17,3 @@ slink "$CONFIGS_PATH/init.vim" "$CONFIG_DIR/init.vim" "Init.vim config"
 
 h2 install plugins
 exe "nvim -c 'PlugInstall' -c ':x' -c ':x'"
-
-# path=$PWD
-
-# . upd.sh
-
-# cd $path
-# sudo pacman -S neovim
-
-# echo "Installing vim-plug pluging manager for neovim ..."
-# sudo pamac install neovim-plug
-# pwd
-
-# echo Set vimrc with 256 colors
-# cp init.vim.256_colors init.vim
-
-# read -p "Any key for setup nvim and install plugins" anykey
-# echo Starting setup_nvim.sh
-# . setup_nvim.sh
