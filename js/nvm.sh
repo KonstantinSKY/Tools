ec90e6f23c595943bec0c8741b418300fa877d32
SKY <sky012877@gmail.com>
2024-04-19 22:57:25 -0700

    nvm.sh - Modified :

--- js/nvm.sh
+++ js/nvm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 . exe.lib
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion (optional)
+
 h1 JS Ecosystem setting
 
 h2 Node remote LTS versions
