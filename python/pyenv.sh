2c632a25174278aa8542b5d44e0d5a7cc0662c6a
SKY <sky012877@gmail.com>
2024-04-19 18:59:17 -0700

    python/pyenv.sh - Modified :

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -18,9 +18,12 @@ exe "pyenv install -l"
 # h2 Installing last version and setting it as global....
 # exe "pyenv install; pyenv global; pyenv versions"
 
-read -p 'Enter Python version number for instalation:' -r number
+read -p 'Enter Python version number for instalation: ' -r number
 
-echo "Additional Python version installing ..."
-exe "pyenv install $number; pyenv global $number; pyenv versions"
+h2 "Additional Python version installing ..."
+exe "pyenv install $number" 
+
+h2 "Set Version $number as global in pyenv" 
+exe "pyenv global $number; pyenv versions"
 
 end
