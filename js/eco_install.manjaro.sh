9fe90ca0c7ad614b5105f204e5f2e96511b86b72
SKY <sky012877@gmail.com>
2024-04-19 22:34:13 -0700

    js/eco_install.manjaro.sh - Modified :

--- js/eco_install.manjaro.sh
+++ js/eco_install.manjaro.sh
@@ -6,6 +6,8 @@
 ############################################################################
 . exe.lib
 
+term=urxvt
+
 h1 JavaScript Eco System Installing
 
 h2 Installing nodejs npm
@@ -18,8 +20,14 @@ h2 Installing nvm
 exe "wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
 
 
-h2 Run nvm and setup node version in additional Terminal window
-exe "chmod 700 setup.sh; urxvt -e ./setup.sh"
+h2 Run nvm and install/uninstall node version in additional Terminal window
+exe "chmod 700 setup.sh; $term -e ./nvm.sh"
+
+h2 Installing Yarn
+exe "sudo npm install --global yarn; yarn --version"
+
+h2 "Installing vue-cli globally (no need if you want to use vite)"
+exe "sudo yarn global add @vue/cli; vue --version"
 
 h2 restarting bash
 exec bash
