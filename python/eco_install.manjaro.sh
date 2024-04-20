a625dc7e4a0b5e524f3265049d34be888dd0089e
SKY <sky012877@gmail.com>
2024-04-19 18:13:44 -0700

    python/eco_install.manjaro.sh - Modified :

--- python/eco_install.manjaro.sh
+++ python/eco_install.manjaro.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 . exe.lib
 
+#dirs
+pyenv_dir="$HOME/.pyenv"
+
 #Scripts
-upd=$TOOLS_PATH/pkgs/upd.sh
-pyenv_sh=$TOOLS_PATH/python/pyenv.sh
+pyenv_scr=$TOOLS_PATH/python/pyenv.sh
+
+#Repos
+pyenv_repo="https://github.com/pyenv/pyenv.git"
 
 h1 Python Ecosystem
 
@@ -12,9 +17,6 @@ exe "python3 --version; which pyhton3" -n
 
 h2 Python versions and which:
 exe "python --version; which pyhton" -n
-
-h2 "Update and upgrade system packages"
-exe "sh $upd"
 
 h2 "Checking for python installing version ..."
 exe "python -V" -n
@@ -28,16 +30,22 @@ h2 "installing Xcode Command Line Tools ..."
 exe "sudo pacman -S --needed --noconfirm base-devel openssl zlib xz tk"
 
 h2 "Cloning and Instaling pyenv ..."
-exe "git clone https://github.com/pyenv/pyenv.git ~/.pyenv; pyenv --version"
+clone $pyenv_repo $pyenv_dir
+pyenv --version
 
-h2 pyenv Running for install python new version
-exe "sh $pyenv_sh"
+h1 Add Python versions to pyenv
+run "$pyenv_scr" "New python version installing"
+
+h1 "pip & pipx"
 
 h2 pip installing
 exe "python -m ensurepip --upgrade; python -m pip --version"
 
 h2 pipx installing
-exe "sudo pacman -S python-pipx; pipx --version"
+add python-pipx
+pipx --version
+
+h1 poetry 
 
 h2 poetry installing
 exe "pipx install poetry; poetry --version"
@@ -47,4 +55,5 @@ exe "pipx install pip-review"
 
 h2 Running pip-review and ubdating modules
 exe "pip-review --auto"
-end "$0"
+
+end
