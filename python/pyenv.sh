348035fd8f14cfb486823791fc103fdb805e80ec
SKY <sky012877@gmail.com>
2024-04-19 18:40:53 -0700

    python/pyenv.sh - Modified :

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -9,17 +9,17 @@
 
 h1 Python Versions pyenv manager
 
-h2 Installed Python versions:
+h2 Showing installed Python versions:
 exe "pyenv versions"
 
 h2 Checking availiable Python versions...
 exe "pyenv install -l"
 
-h2 Installing last version and setting it as global....
-exe "pyenv install; pyenv global; pyenv versions"
+# h2 Installing last version and setting it as global....
+# exe "pyenv install; pyenv global; pyenv versions"
 
 h2 Entering additional Python version
-exe "echo Enter version number:"
+exe "echo Enter version number:" --result
 number=result
 
 echo "Additional Python version installing ..."
