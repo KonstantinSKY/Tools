ef9d3d857392ac7ab07be72b02936fb5795e241f
SKY <sky012877@gmail.com>
2024-04-22 23:37:46 -0700

    git/create_repo_gh.sh - Modified :

--- git/create_repo_gh.sh
+++ git/create_repo_gh.sh
@@ -5,27 +5,20 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description :                                                            #
 ############################################################################
-source ~/Tools/exe.sh
-start $0
+. exe.lib
 
 h1 new GitHub Private repository
 
 h2 Showing existing GitHub repo list
 exe "gh repo list --limit 100"
 
+read_if_not "$1"
+repo_name="$result"
 
-
-if [ $# -lt 1 ]; then
-    	repo_name=""
-	else
-		repo_name="$1 --private --clone"
-fi
-
-h2 "Creating new repo at GitHub directly and clone to $(pwd) directory"
-exe "gh repo create $repo_name"
-
+h2 "Creating new repo at GitHub private directly and clone to $(pwd) directory"
+exe "gh repo create $repo_name --private --clone"
 
 h2 Showing created repo in GitHub list ...
 exe "gh repo list | grep '$1'"
 
-end $0
+end
