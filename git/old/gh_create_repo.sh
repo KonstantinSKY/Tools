dd0f860dbdd0f914fb8063aca8a1736ad3c1543f
SKY <sky012877@gmail.com>
2024-04-22 23:38:32 -0700

    ad:wq

--- git/old/gh_create_repo.sh
+++ git/old/gh_create_repo.sh
@@ -5,33 +5,29 @@
 # Author      : Stan SKY                     E-mail : sky012877@gmail.com  #
 # Description : Create new repository and directory                        #
 ############################################################################
+. exe.lib
 
 license='GNU General Public Licence v3.0'
 
-echo Creating new repo at Github
-echo Showing github repos
-
-echo "gh repo list --limit 100"
-gh repo list --limit 100
+h1 new Github repository
 
-echo Enter repo name
-read repo
-echo Enter repo description
-read descr
+h2 Showing github repos
+exe "gh repo list --limit 100"
 
-echo Will be create new public repo at GitHub: $repo
-echo Anykey for create new repository
-read anykey
+read -p -r "Enter repo name:" repo
+read -p -r "Enter repo description" descr
 
-gh repo create $repo --public --add-readme -d "$descr"
+h2 Creation new public repo at GitHub: "$repo"
+
+gh repo create "$repo" --public --add-readme -d "$descr"
 
 echo all repos listing...
 gh repo list
 echo Creating link to .gitignore
-ln -s $TOOLS_PATH/git/gitignore $repo/.gitignore
+ln -s "$TOOLS_PATH"/git/gitignore "$repo"/.gitignore
 
-echo $repo repository work directory:
-cd $repo
+echo "$repo" repository work directory:
+cd $repo || exit
 ls -la
-ln -s $TOOLS_PATH/git/gitignore $repo/.gitignore
-
+ln -s "$TOOLS_PATH"/git/gitignore "$repo"/.gitignore
+end
