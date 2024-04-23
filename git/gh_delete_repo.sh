fc72a4bf1e737b6055ac10f0bc77e95a4dd3584b
SKY <sky012877@gmail.com>
2024-04-22 23:37:46 -0700

    git/gh_delete_repo.sh - Modified :

--- git/gh_delete_repo.sh
+++ git/gh_delete_repo.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
-
-source $EXE
-start $0
+# shellcheck disable=SC1091
+. exe.lib
 
 h1 Deleting repository from Github...
 
-if [  $1 ]
-	then
-		repo=$1
-	else
-		gh repo list --limit 100
-		echo Enter repo name for deleting:
-		read repo
-	fi
+if [ "$1" ]; then
+	repo=$1
+else
+	gh repo list --limit 100
+	echo Enter repo name for deleting:
+	read -r repo
+fi
 
-echo Will be deleting the repository from GitHub !permanently and forever!: $repo
-echo Enter "YES" if you for deleting $repo forever
-read yes
-if [ ! "yes" == "$yes" |  tr '[:lower:]' '[:upper:]' ]
-	then
-		echo Exiting
-		exit
-	fi
+echo Will be deleting the repository from GitHub !permanently and forever!: "$repo"
+echo Enter "YES" if you for deleting "$repo" forever
+read -r yes
+if [ ! "YES" == "$(echo "$yes" | tr '[:lower:]' '[:upper:]')" ]; then
+	echo "Exiting"
+	exit
+fi
 
 echo Deleting...
 
-gh repo delete $repo --yes
+gh repo delete "$repo" --yes
 
 gh repo list --limit 100
 
-end $0
+end
