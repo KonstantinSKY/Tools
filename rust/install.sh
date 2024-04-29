1fd2bd11ed0cccff095add84f84c58fc0ba60f7e
SKY <sky012877@gmail.com>
2024-04-29 10:47:44 -0700

    rust/install.sh - Modified :

--- rust/install.sh
+++ rust/install.sh
@@ -2,17 +2,19 @@
 
 . exe.lib
 
-H1 "Rust & ECO Instalation"
+h1 "Rust & ECO Instalation"
 
 h2 Installing  Rust
-exe "$ curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh"
+exe "curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh"
 
+h2 Restarting your shell
+exe "su $USER"
 
 h2 Checkink rusup Version
-exe "rustc --version;" -n
+exe "rustc --version" -n
 
 h2 Checkink Cargo Version
-exe "Cargo --version;" -n
+exe "cargo --version" -n
 
 h2 Update to new Version
 exe "rustup update"
