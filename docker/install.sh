fe2bf915ac64664656a7f5c2b096435251b040cb
SKY <sky012877@gmail.com>
2024-07-06 20:35:06 -0700

    exe util update

--- docker/install.sh
+++ docker/install.sh
@@ -12,34 +12,10 @@ install=$TOOLS_PATH/packges/install.sh
 
 
 
-h1 DOCKER SETUP
-
-h2 "Checking if Docker is already installed..."
-docker --version
-docker-compose --version
-
-h2 "Installing Docker & ECO System ..."
-add docker docker-compose docker-buildx
-
-docker --version
-docker-compose --version
-
-h2 Starting Docker systemclt ...
-exe "sudo systemctl start docker" 
-exe "sudo systemctl enable docker"
-
-h2 Show Docker Status
-exe "systemctl status docker --no-pager" -n
-
-
-h2 Adding current user to docker group ...
-exe "sudo usermod -aG docker $USER"
-
-h2  "Restarting SHELL"
-exe "su $USER"
-
-
-echo "Checking Docker by Hello world (test Image) ..."
-exe "docker run hello-world"
+{
+  "terminal.integrated.env.linux": {
+    "DOCKER_HOST": "unix:///var/run/docker.sock"
+  }
+}
 
 end
